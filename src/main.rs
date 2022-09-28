c3d48dd6cd51c15247508efc7969062e65f2d4a3
xinhaoniu <xniu3@ualberta.ca>
2022-09-28 15:28:06 -0600

    Deliverable 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rand::prelude::*;
+use apint::Int;
+use apint::ApInt;
+use primes::is_prime;
+use std::vec::Vec;
+
 fn main() {
-    println!("Hello, world!");
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    let mut rng = thread_rng();
+    // rng.gen is a generic function randomly generate True or False
+    // That's why sometimes the results are true, and sometimes false. 
+    if rng.gen() {
+        let x: f64 = rng.gen();
+        let y = rng.gen_range(-10.0, 10.0);
+        println!("x is: {}", x);
+        println!("y is: {}", y);
+        println!("Random Number between 0 and 9: {}", rng.gen_range(0, 10));
+    }
+    else{
+        println!("rng.gen() is false");
+    }
+    
 }
