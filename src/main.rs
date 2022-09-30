82ea16ebbf1fd1d72711501bb569ba1fb40b96bc
xinhaoniu <xniu3@ualberta.ca>
2022-09-30 14:09:21 -0600

    Question 4 Solved

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,24 @@
 use std::mem;
 
-#[derive(Debug)]
-struct Bag1<u8> {
-    t: [u8; 3],
-}
-
-fn Bag1Size<u8>(bag:&Bag1<u8>) -> usize{
-    let mut size = 0;
-    for i in bag.t.iter(){
-        size += mem::size_of_val(i);
-    }
-    return size;
-}
-
-#[derive(Debug)]
-struct Bag2<u32> {
-    t: [u32; 3],
-}
-
-fn Bag2Size<u32>(bag:&Bag2<u32>) -> usize{
-    let mut size = 0;
-    for i in bag.t.iter(){
-        size += mem::size_of_val(i);
-    }
-    return size;
-}
-
-
 fn main() {
-    let b1 = Bag1 {t: [1u8, 2u8, 3u8], };
-    let b2 = Bag2 {t: [1u32, 2u32, 3u32], };
-    println!("size of First Bag is {:?} bytes ",Bag1Size(&b1));
-    println!("size of Second Bag is {:?} bytes ",Bag2Size(&b2));
+    let vec1 = vec![12, 32, 13];
+    let vec2 = vec![44, 55, 16];
+    { 
+        let vec1_iter = vec1.iter(); 
+        //VecSize);
+        println!("{}",mem::size_of_val(&vec1_iter));
+    }
+
+    {
+        let vec_chained = vec1.iter().chain(vec2.iter());
+        println!("{}",mem::size_of_val(&vec_chained));
+    }
+
+    { 
+        let vec1_2=vec![vec1, vec2];
+        let vec_flattened = vec1_2.iter().flatten(); 
+        println!("{}",mem::size_of_val(&vec_flattened));
+    }
 }
 
 
---
+++ target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/xinhaoniu/Projects/RustAsgn2/target/debug/hello_world: /Users/xinhaoniu/Projects/RustAsgn2/src/main.rs
