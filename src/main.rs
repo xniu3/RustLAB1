0743bcfcb15954997d7ca622d456b950c0f7b1ad
xinhaoniu <xniu3@ualberta.ca>
2022-09-30 14:36:05 -0600

    Question 5 Solved

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 use std::mem;
 
+
 fn main() {
     let vec1 = vec![12, 32, 13];
     let vec2 = vec![44, 55, 16];
     { 
         let vec1_iter = vec1.iter(); 
-        //VecSize);
-        println!("{}",mem::size_of_val(&vec1_iter));
+        
+        println!("{}",mem::size_of_val(&Box::from(vec1_iter)));
     }
 
     {
         let vec_chained = vec1.iter().chain(vec2.iter());
-        println!("{}",mem::size_of_val(&vec_chained));
+        println!("{}",mem::size_of_val(&Box::from(&vec_chained)));
     }
 
     { 
         let vec1_2=vec![vec1, vec2];
         let vec_flattened = vec1_2.iter().flatten(); 
-        println!("{}",mem::size_of_val(&vec_flattened));
+        println!("{}",mem::size_of_val(&Box::from(&vec_flattened)));
     }
 }
 
